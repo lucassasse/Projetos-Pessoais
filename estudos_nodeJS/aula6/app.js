5ef52b614aaf3fe6de6215d7df91e46f87088df0
Lucas Eduardo Sasse <lucas1sasse@gmail.com>
2022-05-18 21:53:42 -0300

    update estudos_nodejs

--- estudos_nodeJS/aula6/app.js
+++ estudos_nodeJS/aula6/app.js
@@ -1,17 +1,67 @@
 const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const moment = require('moment')
+const Pagamento = require('./models/Pagamento')
 
-app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }))
+app.engine(
+  'handlebars',
+  handlebars.engine({
+    defaultLayout: 'main',
+    helpers: {
+      formatDate: date => {
+        return moment(date).format('DD/MM/YYYY')
+      }
+    },
+    runtimeOptions: {
+      allowProtoPropertiesByDefault: true,
+      allowProtoMethodsByDefault: true
+    }
+  })
+)
 app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 //Rotas
 app.get('/pagamento', function (req, res) {
-  res.render('pagamento')
+  Pagamento.findAll({ order: [['id', 'DESC']] }).then(function (pagamentos) {
+    res.render('pagamento', { pagamentos: pagamentos })
+  })
 })
 
-app.get('/add-pagamento', function (req, res) {
-  res.render('add-pagamento')
+app.get('/cad-pagamento', function (req, res) {
+  res.render('cad-pagamento')
+})
+
+app.post('/add-pagamento', function (req, res) {
+  Pagamento.create({
+    nome: req.body.nome,
+    valor: req.body.valor
+  })
+    .then(function () {
+      //res.send('Pagamento cadastrado com sucesso!')
+      res.redirect('/pagamento')
+    })
+    .catch(function (erro) {
+      res.send('Erro ao cadastrar pagamento: ' + erro)
+    })
+  //res.send('Nome: ' + req.body.nome + '<br> Valor: ' + req.body.valor + '<br>')
+})
+
+app.get('/del-pagamento/:id', function (req, res) {
+  Pagamento.destroy({
+    where: { id: req.params.id }
+  })
+    .then(function () {
+      res.redirect('/pagamento')
+      //res.send('Pagamento removido com sucesso!')
+    })
+    .catch(function () {
+      res.send('Erro ao remover pagamento')
+    })
 })
 
 app.listen(8080)

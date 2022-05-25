23d79fce13ceed04b19c86cdb29c4a50fd694b96
Lucas Eduardo Sasse <lucas.sasse@pradi.local>
2022-05-25 15:16:28 -0300

    update estudos_react

--- estudos_react/src/App.js
+++ estudos_react/src/App.js
@@ -1,10 +1,23 @@
 import './App.css'
+import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
 
 function App() {
+  const nome = 'Fulano'
+
   return (
     <div className="App">
-      <h1>Olá React</h1>
-      <p>Meu primeiro App</p>
+      <HelloWorld />
+      <SayMyName nome="João" />
+      <SayMyName nome="Maria" />
+      <SayMyName nome={nome} />
+      <Pessoa
+        nome="Rodrigo"
+        idade="28"
+        profissao="Programador"
+        foto="https://via.placeholder.com/150"
+      />
     </div>
   )
 }

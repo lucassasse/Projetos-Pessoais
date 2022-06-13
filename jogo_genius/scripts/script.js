de0373cb1d3a0ab823e4ea3640902c868889d61b
Lucas Eduardo Sasse <lucas.sasse@pradi.local>
2022-06-13 17:51:34 -0300

    update genius

--- jogo_genius/scripts/script.js
+++ jogo_genius/scripts/script.js
@@ -19,15 +19,17 @@ points = 0
 
 function getColor(color) {
   if (color.id === 'blue') {
-    actualGameClick.push(0)
+    actualGameClick.push('0')
   } else if (color.id === 'yellow') {
-    actualGameClick.push(1)
+    actualGameClick.push('1')
   } else if (color.id === 'green') {
-    actualGameClick.push(2)
+    actualGameClick.push('2')
   } else {
-    actualGameClick.push(3)
+    actualGameClick.push('3')
   }
   console.log(actualGameClick)
+
+  compareArray()
 }
 
 function play() {
@@ -35,28 +37,23 @@ function play() {
   console.log(actualGame)
 }
 
-/*window.setTimeout(function flashColor(pos) {
-  if (actualGame[pos] === 0) {
-    blue.style.backgroundColor = 'blue'
-  } else if (actualGame[pos] === 1) {
-    yellow.style.backgroundColor = 'yellow'
-  } else if (actualGame[pos] === 2) {
-    green.style.backgroundColor = 'green'
-  } else {
-    red.style.backgroundColor = 'red'
-  }
-}, 1000)*/
-
-/*function compareArrays() {
-  if (actualGame[pos] === actualGameClick[pos]) {
-    play()
-  } else {
-    fail()
-  }
-}*/
-
 function randomColor() {
   return (Math.random() * (3 - 0) + 0).toFixed(0)
+}
+
+function compareArray() {
+  if (actualGameClick.length !== actualGame.length) return false
+
+  for (var i = 0, len = actualGame.length; i < len; i++) {
+    if (actualGame[i] === actualGameClick[i]) {
+    } else {
+      fail()
+    }
+  }
+  console.log('true')
+  points++
+  pointsOutput.innerHTML = points
+  actualGameClick = []
 }
 
 function fail() {

6df5eb2d9a0200adf2af7800923977df5573b3a0
Lucas Eduardo Sasse <lucas.sasse@pradi.local>
2022-06-23 17:51:08 -0300

    create a barras_de_progresso

---
+++ barras_de_progresso/script.js
@@ -0,0 +1,25 @@
+function update() {
+  var element = document.getElementById('myprogressBar')
+  var width = 1
+  var identity = setInterval(scene, 50)
+
+  function scene() {
+    if (width >= 100) {
+      clearInterval(identity)
+    } else {
+      width++
+      element.style.width = width + '%'
+      element.innerHTML = width * 1 + '%'
+    }
+  }
+}
+
+/*
+if (width >= 100) {
+      clearInterval(identity)
+    } else {
+      width++
+      element.style.width = width + '%'
+      element.innerHTML = width * 1 + '%'
+    }
+*/

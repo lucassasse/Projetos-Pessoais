763287038cbd7b4bd74ccfa769a497ecc9d02543
Lucas Eduardo Sasse <lucas1sasse@gmail.com>
2022-06-27 22:58:21 -0300

    create todo-list

---
+++ todo_list/scripts.js
@@ -0,0 +1,9 @@
+const inputTxt = document.querySelector('#input')
+const divOutput = document.querySelector('#output')
+
+let tasks = []
+
+console.log(inputTxt)
+console.log(divOutput)
+
+function save() {}

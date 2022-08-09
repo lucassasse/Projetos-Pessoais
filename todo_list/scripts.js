bb940a5b7a8da6c69e2bdd09e03a29ff02eaf4ce
Lucas Eduardo Sasse <lucas.sasse@pradi.local>
2022-08-09 10:39:02 -0300

    update script

--- todo_list/scripts.js
+++ todo_list/scripts.js
@@ -1,82 +1,84 @@
-//função do botão X = excluir de fato o item do array.
+let tasks = []
 
-//Salvar e buscar a lista do localStorage ao invés do array local
-
-input = document.getElementById('input')
-let listTask = ['Comprar pão', 'Lavar a louça', 'Varrer a casa']
-
-// Start existing to-do list
-
-function initListTasks() {
-  for (i = 0, len = listTask.length; i < len; i++) {
-    let li = document.createElement('li')
-    let p = document.createElement('p')
-    let text = document.createTextNode(listTask[i])
-
-    li.appendChild(p)
-    p.appendChild(text)
-
-    document.getElementById('ulOutput').appendChild(li)
+sendEnter = document.getElementById('inputTxt')
+sendEnter.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    e.preventDefault()
+    getTask()
   }
-}
-initListTasks()
+})
 
-// Create a new list item when clicking on the "Salvar" button
-function addTask() {
-  if (input.value == '') {
+function getTask() {
+  let inputTxt = document.getElementById('inputTxt')
+
+  if (inputTxt.value == '') {
     alert('Enter a task.')
     stop
   } else {
-    listTask.push(input.value)
-    listTasks()
+    tasks.unshift(inputTxt.value)
+    updateLocalStorage()
   }
-
-  input.value = ''
-  input.focus()
+  inputTxt.value = ''
+  inputTxt.focus()
 }
 
-function listTasks() {
-  let li = document.createElement('li')
-  let p = document.createElement('p')
-  let text = document.createTextNode(listTask[listTask.length - 1])
-
-  li.appendChild(p)
-  p.appendChild(text)
-
-  document.getElementById('ulOutput').appendChild(li)
-
-  let span = document.createElement('SPAN')
-  let txt = document.createTextNode('X')
-  span.className = 'close'
-  span.appendChild(txt)
-  li.appendChild(span)
+function updateLocalStorage() {
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+  printTasks()
 }
 
-// Create a "close" button and append it to each list item
-let myNodelist = document.getElementsByTagName('li')
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement('SPAN')
-  var txt = document.createTextNode('X')
-  span.className = 'close'
-  span.appendChild(txt)
-  myNodelist[i].appendChild(span)
+function printTasks() {
+  let localStorageTasks = JSON.parse(localStorage.getItem('tasks'))
+
+  if (localStorageTasks) {
+    let ul = document.getElementById('tasks')
+    ul.innerHTML = ''
+    for (let i = 0; i < localStorageTasks.length; i++) {
+      let li = document.createElement('li')
+      li.innerHTML = localStorageTasks[i]
+      ul.appendChild(li)
+    }
+    tasks = localStorageTasks
+  }
+  createDeleteButton()
+}
+printTasks()
+
+function createDeleteButton() {
+  let myTasklist = document.getElementsByTagName('li')
+  for (i = 0; i < myTasklist.length; i++) {
+    var span = document.createElement('button')
+    var txt = document.createTextNode('X')
+    span.className = 'close'
+    span.appendChild(txt)
+    myTasklist[i].appendChild(span)
+  }
+  deleteTask()
 }
 
-// Click on a close button to hide the current list item
-let close = document.getElementsByClassName('close')
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function () {
-    let div = this.parentElement
-    div.style.display = 'none'
+function deleteTask() {
+  let close = document.getElementsByClassName('close')
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement
+      div.parentElement.removeChild(div)
+      tasks.splice(tasks.indexOf(div.innerText.replace('X', '')), 1)
+      updateLocalStorage()
+    }
   }
 }
 
-// Add a "checked" symbol when clicking on a list item
+function clearTasks() {
+  localStorage.clear()
+  location.reload()
+}
+
 let list = document.querySelector('ul')
+console.log(list)
 list.addEventListener(
   'click',
   function (e) {
-    if (e.target.tagName === 'P') {
+    if (e.target.tagName === 'LI') {
       e.target.classList.toggle('checked')
     }
   },

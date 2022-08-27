c4f033202837367f3cd6935c5c1dfe666d3172a9
Lucas Eduardo Sasse <lucas1sasse@gmail.com>
2022-08-27 15:54:57 -0300

    updade conversores

--- conversores/scripts.js
+++ conversores/scripts.js
@@ -1,5 +1,22 @@
-decimalNumber = document.getElementById("inputDecimal")
+const decimalNumberInput = document.getElementById('inputDecimal')
 
-function convert(){
+function toBinary() {
+  let result = []
+  let decimalNumber = decimalNumberInput.value
 
+  let num = 3
+  while (decimalNumber > 2) {
+    num = decimalNumber % 2
+    decimalNumber = parseInt(decimalNumber / 2)
+    quociente = Math.floor(decimalNumber / 2)
+
+    result.unshift(num)
+
+    console.log('num: ' + num)
+    console.log('decimalNumber: ' + decimalNumber)
+    console.log('quociente: ' + quociente)
+  }
+
+  result.unshift(quociente % 2)
+  console.log('result> ' + result)
 }

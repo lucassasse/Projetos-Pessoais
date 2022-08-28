736ea8d0a3cac4bdda241d33cef87dda2a9ae8f7
Lucas Eduardo Sasse <lucas1sasse@gmail.com>
2022-08-27 22:19:18 -0300

    updade conversores

--- conversores/scripts.js
+++ conversores/scripts.js
@@ -1,22 +1,53 @@
 const decimalNumberInput = document.getElementById('inputDecimal')
 
-function toBinary() {
+/*function toBinary() {
   let result = []
   let decimalNumber = decimalNumberInput.value
 
-  let num = 3
-  while (decimalNumber > 2) {
+  let num = 2
+  while (decimalNumber > 1) {
     num = decimalNumber % 2
     decimalNumber = parseInt(decimalNumber / 2)
     quociente = Math.floor(decimalNumber / 2)
 
     result.unshift(num)
-
-    console.log('num: ' + num)
-    console.log('decimalNumber: ' + decimalNumber)
-    console.log('quociente: ' + quociente)
   }
 
-  result.unshift(quociente % 2)
-  console.log('result> ' + result)
+  if (quociente % 2 == 0) {
+    result.unshift(1)
+  } else {
+    result.unshift(2)
+  }
+  console.log('result: ' + result)
+  printResult(result, 1)
+}*/
+
+function toBinary(){
+  result = parseInt(decimalNumberInput.value).toString(2)
+  console.log(result)
+}
+
+function toOctal() {
+  let result = []
+  let decimalNumber = decimalNumberInput.value
+
+  let num = 10
+  while (decimalNumber > 7) {
+    num = decimalNumber % 8
+    decimalNumber = parseInt(decimalNumber / 8)
+    quociente = Math.floor(decimalNumber / 8)
+
+    result.unshift(num)
+  }
+
+  if (quociente % 2 == 0) {
+    result.unshift(1)
+  } 
+  console.log('result: ' + result)
+  printResult(result, 2)
+}
+
+function printResult(result, type) {
+  type = type == 1 ? 'Binary: ' : 'Octal: '
+  document.getElementById('result').innerHTML = type + result.join('')
 }

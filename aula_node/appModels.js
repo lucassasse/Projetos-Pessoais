a76d26fe7e1ebf9383dcc3bcc34227bd25760e63
Lucas Eduardo Sasse <lucas1sasse@gmail.com>
2022-10-24 23:53:39 -0300

    update aula_node

---
+++ aula_node/appModels.js
@@ -0,0 +1,45 @@
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('test', 'root', '52e0240a', {
+  host: 'localhost',
+  dialect: 'mysql'
+})
+
+const Postagem = sequelize.define('postagens', {
+  titulo: {
+    type: Sequelize.STRING
+  },
+  conteudo: {
+    type: Sequelize.TEXT
+  }
+})
+
+//Postagem.sync({ force: true })
+
+/*Postagem.create({
+  titulo: 'Um Título!',
+  conteudo: 'Aqui vai o conteúdo sbla bla bla...'
+})*/
+
+const Usuario = sequelize.define('usuarios', {
+  nome: {
+    type: Sequelize.STRING
+  },
+  sobrenome: {
+    type: Sequelize.STRING
+  },
+  idade: {
+    type: Sequelize.INTEGER
+  },
+  email: {
+    type: Sequelize.STRING
+  }
+})
+
+//Usuario.sync({ force: true })
+
+Usuario.create({
+  nome: 'Lucas Eduardo',
+  sobrenome: 'Sasse',
+  idade: 24,
+  email: 'lucas1sasse@gmail.com'
+})

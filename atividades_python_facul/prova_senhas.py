9edaeac2edf798e4e7bed4d2d9a593d20efe259b
Lucas Eduardo Sasse <lucas1sasse@gmail.com>
2022-11-24 11:30:11 -0300

    add prova_senha in python

--- atividades_python_facul/prova_senhas.py
+++ atividades_python_facul/prova_senhas.py
@@ -1,40 +1,58 @@
 import random
-ranking = [["vazio", 0], ["vazio", 0], ["vazio", 0]]
+ranking = [["Lucas", 4], ["Marco", 7], ["Pedro", 8]]
+jogadorAtual = []
 
-tentativas = 0
+def rePlay():
+    print("Suas tentativas acabaram! Deseja Jogar novamente?")
+    escolha = input("1 - Sim, mesmo jogador! | 2 - Sim, outro jogador! | 3 - Não!")
+    if escolha == "1":
+        sortearNumeros()
+    elif escolha == "2":
+        novoJogador()
+    elif escolha == "3":
+        print("Ok! Até a próxima!")
+    else:
+        print("Por favor, selecione uma opção válida.")
+        rePlay()
+
+def novoJogador():
+    global jogadorAtual
+    jogadorAtual = []
+    jogadorAtual.append(input("Qual seu nome?"))
+    sortearNumeros()
+    
 def sortearNumeros():
     numerosSorteados = []
     while len(numerosSorteados) < 4:
-        numerosSorteados.append(random.randrange(1,7))
-    tentativa(numerosSorteados)
+        numSorteado = random.randint(1, 7)
+        if numSorteado not in numerosSorteados:
+            numerosSorteados.append(numSorteado)
+    print(numerosSorteados)
+    chutes(numerosSorteados)
     
-def tentativa(numerosSorteados):
-    chute = input("Digite 4 números: (ou 'sair')")
-    verificacaoPrimária(chute, numerosSorteados)
+def chutes(numerosSorteados):
+    tentativas = 0
+    while tentativas < 3:
+        chute = input("Digite 4 números ou 'sair'")
+        if chute == "sair":
+            print("Você desistiu! Os números sorteados eram: ", numerosSorteados)
+            break
+        else:
+            tentativas += 1
+            verificacao(chute)
+    if tentativas == 10:
+        fimDeJogo()
 
-def verificacaoPrimária(chute, numerosSorteados):
-    listaNumerosChute = []
-    listaNumerosChute = list(chute)
-    if chute == "sair":
-        print("Fim de jogo. Os números eram: ", numerosSorteados)
-        logicaRank()
-    elif tentativas >= 10:
-        print("Suas tentativas acabaram!")
-    elif (len(chute) < 4) or (len(chute) > 4):
-        print("Digite 4 números.")
-    elif not chute.isdigit():
-        print("Digite apenas números.")
-    else:
-        print("Passou!")
-        tentativas += 1
-        verificacaoNumeros(chute, listaNumerosChute, numerosSorteados)
+def verificacao(chute):
+    print(chute)
+    
+def fimDeJogo(tentativas):
+    print("Seu total de tentativas foi de: ", tentativas)
+    jogadorAtual.append(tentativas)
+    rank()
 
-def verificacaoNumeros(chute, listaNumerosChute, numerosSorteados):
-    print(chute, listaNumerosChute, numerosSorteados)
-    logicaRank()
+def rank():
+    print(jogadorAtual)
+    rePlay()
 
-def logicaRank():
-    ranking.sort()
-    print("Ranking", ranking)
-
-sortearNumeros()
+novoJogador()

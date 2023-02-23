ac52066555000de432ae11e1d11e6eb4673dd6ff
Lucas Eduardo Sasse <lucas1sasse@gmail.com>
2023-02-23 17:23:47 -0300

    update Forest component

--- mini_rpg_2/src/pages/Forest.jsx
+++ mini_rpg_2/src/pages/Forest.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Header } from '../components/Header'
 import { usePlayer } from '../components/usePlayer'
 import '../styles/Main.css'
@@ -5,14 +6,45 @@ import '../styles/Main.css'
 export const Forest = () => {
   const { player, setPlayer } = usePlayer()
 
+  const [monster, setMonster] = useState({
+    strength: 10,
+    life: 10,
+    number: 10
+  })
+
+  function randomNumber(num) {
+    return Math.floor(Math.random() * (num - 10) + 10)
+  }
+
+  const battle = () => {
+    monster.life = 0
+    if (monster.life <= 0) {
+      alert('Parabéns, você matou o monstro. Um mais forte aparece!')
+      setPlayer({
+        ...player,
+        money: player.money + 10
+      })
+      setMonster({
+        ...monster,
+        strength: (monster.strength = randomNumber(monster.number)),
+        life: (monster.life = randomNumber(monster.number)),
+        number: monster.number * 1.5
+      })
+      console.log({ ...monster })
+    }
+  }
+
   return (
     <div>
       <Header />
       <h1 className="title">FOREST</h1>
-      <button className="button" disabled={true}>
-        Batle
+      <p>Monster strength: {monster.strength}</p>
+      <button onClick={battle} className="button" disabled={player.money < 10}>
+        Batle: 10 gold
       </button>
-      <p>Aqui haverá um sistema de batalhas!</p>
+      <p>Monster Life: {monster.life}</p>
+      <hr />
+      <p>Sistema de batalhas em desenvolvimento!</p>
     </div>
   )
 }

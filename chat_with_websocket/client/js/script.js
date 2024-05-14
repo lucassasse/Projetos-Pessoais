af0717d0519f7fd9d8ecf3b45b528a8c4785bea0
unknown <lucas1sasse@gmail.com>
2024-05-14 09:49:30 -0300

    finish the chat project

--- chat_with_websocket/client/js/script.js
+++ chat_with_websocket/client/js/script.js
@@ -5,6 +5,9 @@ const loginInput = document.querySelector('.login__input');
 const chat = document.querySelector('.chat');
 const chatForm = document.querySelector('.chat__form');
 const chatInput = document.querySelector('.chat__input');
+const chatMessages = document.querySelector('.chat__messages');
+
+let ws;
 
 const user = { 
     id: '', 
@@ -12,12 +15,47 @@ const user = {
     color: '' 
 };
 
-let ws;
-
 const getRandomColor = () => `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, '0')}`;
 
-const processMessage = (e) => {
-    console.log(e.data);
+const scrollScreen = () => {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+}
+
+const createMessageSelfElement = (content) => {
+    const div = document.createElement('div');
+
+    div.classList.add('message--self');
+    div.innerHTML = content;
+
+    return div;
+}
+
+const createMessageOtherElement = (content, sender, senderColor) => {
+    const div = document.createElement('div');
+    const span = document.createElement('span');
+
+    div.classList.add('message--other');
+    div.classList.add('message--self');
+    span.classList.add('message--sender');
+
+    span.style.color = senderColor;
+
+    div.appendChild(span);
+
+    span.innerHTML = sender;
+    div.innerHTML += content;
+
+    return div;
+}
+
+const processMessage = ({ data }) => {
+    const { userId, userName, userColor, content } = JSON.parse(data);
+
+    const element = userId === user.id ? createMessageSelfElement(content) : createMessageOtherElement(content, userName, userColor);
+
+    chatMessages.appendChild(element);
+
+    scrollScreen();
 }
 
 const handleLogin = (e) => {
@@ -32,15 +70,21 @@ const handleLogin = (e) => {
 
     ws = new WebSocket('ws://localhost:8080');
     ws.onmessage = processMessage;
-    // ws.onopen = () => ws.send(`Usuário: "${user.name}" entrou no chat!`);
-    
-    console.log(user);
 };
 
 const sendMessage = (e) => {
     e.preventDefault();
 
-    ws.send(chatInput.value);
+    const message = {
+        userId: user.id,
+        userName: user.name,
+        userColor: user.color,
+        content: chatInput.value
+    };
+
+    ws.send(JSON.stringify(message));
+
+    chatInput.value = '';
 };
 
 loginForm.addEventListener('submit', handleLogin);
